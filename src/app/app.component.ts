436f7655dffcddd568c6bb7df23c6b17827d128c
WebZenSai <fabian.zeussel@gmx.de>
2025-09-18 00:39:02 +0200

    refac: cleaned up scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HowToOrderComponent } from './components/how-to-order/how-to-order.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { LocationComponent } from './components/location/location.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    HeroComponent,
+    FooterComponent,
+    HowToOrderComponent,
+    MenuComponent,
+    LocationComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Sakura Ramen';
