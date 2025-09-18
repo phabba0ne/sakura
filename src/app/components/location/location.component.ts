707cf245a16ce9d42695efa26e5b80db8477807b
WebZenSai <fabian.zeussel@gmx.de>
2025-09-18 03:25:32 +0200

    fix: major fixes on project structure image paths buggy

--- src/app/components/location/location.component.ts
+++ src/app/components/location/location.component.ts
@@ -1,8 +1,10 @@
 import { Component, signal } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-location',
   standalone: true,
+  imports: [CommonModule],
   template: `
     <section class="find-us-at-section" id="location">
       <div class="content">
@@ -12,7 +14,7 @@ import { Component, signal } from '@angular/core';
           <span>{{ address() }}</span>
         </div>
       </div>
-      <iframe [src]="mapUrl()" [title]="mapTitle()" loading="lazy"> </iframe>
+      <iframe [src]="mapUrl" [title]="mapTitle()" loading="lazy"></iframe>
     </section>
   `,
   styleUrls: ['./location.component.scss'],
@@ -23,7 +25,11 @@ export class LocationComponent {
   markerAlt = signal('Unseren Hauptstandort finden Sie in München');
   address = signal('Kapuzinerstraße 31, München 80337');
   mapTitle = signal('Google Maps Karte mit unserem Standort in München');
-  mapUrl = signal(
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2663.2109956986274!2d11.558694776809554!3d48.125454252004666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479ddf44cf7e4d5f%3A0x55a7bd29a73e9161!2sKapuzinerstra%C3%9Fe%2031%2C%2080337%20M%C3%BCnchen!5e0!3m2!1sen!2sde!4v1749189400655!5m2!1sen!2sde'
-  );
+
+  mapUrl: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {
+    const url = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!...'; // volle URL hier einfügen
+    this.mapUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 }
